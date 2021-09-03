56788f90c69e2408d827a2d689c753c299f0472c
Lucas Hahn <55736681+lucash-0@users.noreply.github.com>
2021-09-03 14:19:13 +1000

    Updated folder structure and run scripts
    Updated folder structure to:
    - views (component views)
    - layouts (controller that shows view components)
    - components (components within both layout and views)
    - assets (css, jss, imgs etc)
    
    Also added npm run install-all, runs install on all relevant folders.

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,22 +1,17 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import login from './users/login';
-import userManagement from './users/user-management'; 
-import property from './properties/property';
-function routes(){
-    return(
-        <div>
-            <Switch>
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import login from "./views/users/login";
+import userManagement from "./views/users/user-management";
+import property from "./views/properties/property";
+
+function routes() {
+  return (
+    <div>
+      <Switch>
         <Route exact path="/login" component={login}></Route>
         <Route exact path="/user-manage" component={userManagement}></Route>
-        
-            </Switch>
-        </div>
-    )
+      </Switch>
+    </div>
+  );
 }
 export default routes;
