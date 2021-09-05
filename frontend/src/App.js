eea1af737c75a3ec431ece6e6d8ed0c942dbafbe
Lucas Hahn <55736681+lucash-0@users.noreply.github.com>
2021-09-05 17:52:09 +1000

    CSS, Routing and Layouts
    Installed tailwind css, Improved routing, added dashboard layout and tailwind application shell.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-import {  } from "@headlessui/react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect,
 } from "react-router-dom";
-import routes from './routes';
+
+import DashboardLayout from "./layouts/DashboardLayout.js";
+import AuthLayout from "./layouts/AuthLayout.js";
+
+import routes from "./routes";
+
 function App() {
+  /*const getRoutes = () => {
+    return routes.map((route) => {
+      if(route.layout === "dashboardLayout"){
+        <Route exact path={route.pathname} render={(props) => {
+          <DashboardLayout {...props} {...}
+        }
+        }
+      }
+    });
+  };*/
+
   return (
-    <div className="">
-     <div className="">
-       <Router>
-         <ul>
-           <li><a><Link to="login">Login</Link></a></li>
-           <li><a><Link to="/user-manage">Manage</Link></a></li>
-         </ul>
-         <routes/>
-       </Router>
-     </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path={routes
+            .filter((route) => route.layout === "dashboardLayout")
+            .map((route) => route.pathname)}
+          render={(rest) => <DashboardLayout {...rest} />}
+        />
+        <Route
+          exact
+          path={routes
+            .filter((route) => route.layout === "authLayout")
+            .map((route) => route.pathname)}
+          render={(rest) => <AuthLayout {...rest} />}
+        />
+        <Redirect from="/" to="/properties" />
+      </Switch>
+    </Router>
   );
+
+  /*return (
+    <div className="">
+      <div className="">
+        <Router>
+          <ul>
+            <li>
+              <a>
+                <Link to="login">Login</Link>
+              </a>
+            </li>
+            <li>
+              <a>
+                <Link to="/user-manage">Manage</Link>
+              </a>
+              <p>Text Q</p>
+            </li>
+          </ul>
+          <routes />
+        </Router>
+      </div>
+    </div>
+  );*/
 }
 
 export default App;
