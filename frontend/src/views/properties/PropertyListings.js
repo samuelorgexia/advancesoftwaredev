a09dfabed2ab9c4d0a9cb96409c17cddf1a3eb66
Lucas Hahn <55736681+lucash-0@users.noreply.github.com>
2021-09-07 22:38:24 +1000

    Only show view listing button when inside preview

--- frontend/src/views/properties/PropertyListings.js
+++ frontend/src/views/properties/PropertyListings.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import Properties from "../../components/Properties/Properties.js";
 import Slideover from "../../components/Slideover/Slideover.js";
-import Property from "../../views/Properties/Property.js";
+import Property from "./Property.js";
 
 export default function PropertyListings(props) {
   const { setOverrideTitle, match } = props;
