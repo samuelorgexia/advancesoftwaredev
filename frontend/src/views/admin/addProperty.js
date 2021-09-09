c2cc187038800c1d2df13a37768aa2bb8b8d2308
John McCreanor <john.mccreanor@5c6e.com>
2021-09-09 18:43:17 +1000

    rebuild admin pages

---
+++ frontend/src/views/admin/addProperty.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+
+export default function addProperty(){
+    return  (
+        <div>
+            <form>
+                <div>
+                    <label class="block text-gray-700 text-sm font-bold mt-2" for="address">
+                        Address
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="address" type="text" placeholder="Address"/>
+                </div>
+                <div>
+                    <label class="block text-gray-700 text-sm font-bold mt-2" for="postcode">
+                        Postcode
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="postcode" type="text" placeholder="Postcode"/>
+                </div>
+                <div>
+                    <label class="block text-gray-700 text-sm font-bold mt-2" for="listing-name">
+                        Listing Name
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="listing-name" type="text" placeholder="Listing name"/>
+                </div>
+                <div>
+                    <label class="block text-gray-700 text-sm font-bold mt-2" for="property-type">
+                        Property Type
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="property-type" type="text" placeholder="Property type"/>
+                </div>
+                <div>
+                    <label class="block text-gray-700 text-sm font-bold mt-2" for="description">
+                        Description
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" type="text" placeholder="Description"/>
+                </div>
+                <div>
+                    <label class="block text-gray-700 text-sm font-bold mt-2" for="auction-date">
+                        Auction Date
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="auction-date" type="text" placeholder="Auction date"/>
+                </div>
+                <div>
+                    <label class="block text-gray-700 text-sm font-bold mt-2" for="starting-price">
+                        Starting Price
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="starting-price" type="text" placeholder="Starting price"/>
+                </div>
+                <div>
+                    <label class="block text-gray-700 text-sm font-bold mt-2" for="auctioneer">
+                        Auctioneer
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="auctioneer" type="text" placeholder="Auctioneer"/>
+                </div>
+                <div>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-6" type="button">
+                        Create Auction
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+/*
+Address
+Postcode
+listing name
+property type
+description
+auction date
+starting price
+auctioneer
+*/
