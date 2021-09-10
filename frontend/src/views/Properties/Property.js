9234a442edac5eace407315bed0354f155870905
Lucas Hahn <55736681+lucash-0@users.noreply.github.com>
2021-09-10 12:32:53 +1000

    Fixing Properties directory

---
+++ frontend/src/views/Properties/Property.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Button from "../../components/Buttons/Button.js";
+
+export default function Property(props) {
+  // gets the preview variable from props
+  const { preview } = props;
+
+  /*if (preview) {
+    return <div>show in preview</div>;
+  }*/
+
+  //shows when not preview
+  return (
+    <div className="prose">
+      <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/suburban-house-royalty-free-image-1584972559.jpg" />
+
+      <div class="w-full">
+        <h3 class="text-2xl font-medium">
+          8/31 CHARLES STREET, FOREST LODGE, NSW, 2037
+        </h3>
+        <div class="mt-4">
+          <div class="flex flex-col sm:flex-row justify-between items-center sm:items-start py-3 border-t border-gray-300 last:border-none">
+            <span class="w-full sm:w-1/3 font-medium text-center sm:text-left">
+              Auction Location
+            </span>
+            <span class="flex-1 text-center sm:text-right">
+              UTS Building 11
+            </span>
+          </div>
+          <div class="flex flex-col sm:flex-row justify-between items-center sm:items-start py-3 border-t border-gray-300 last:border-none">
+            <span class="w-full sm:w-1/3 font-medium text-center sm:text-left">
+              Live Auction
+            </span>
+            <span class="flex-1 text-center sm:text-right">YES</span>
+          </div>
+          <div class="flex flex-col sm:flex-row justify-between items-center sm:items-start py-3 border-t border-gray-300 last:border-none">
+            <span class="w-full sm:w-1/3 font-medium text-center sm:text-left">
+              Register To BID
+            </span>
+            <span class="flex-1 text-center sm:text-right">NOW</span>
+          </div>
+          <div class="flex flex-col sm:flex-row justify-between items-center sm:items-start py-3 border-t border-gray-300 last:border-none">
+            <span class="w-full sm:w-1/3 font-medium text-center sm:text-left">
+              Auction Date
+            </span>
+            <span class="flex-1 text-center sm:text-right">
+              October 31st, 5:00PM
+            </span>
+          </div>
+          <div class="flex flex-col sm:flex-row justify-between items-center sm:items-start py-3 border-t border-gray-300 last:border-none">
+            <span class="w-full sm:w-1/3 font-medium text-center sm:text-left">
+              Timezone
+            </span>
+            <span class="flex-1 text-center sm:text-right">
+              Australian Eastern Standard Time
+            </span>
+          </div>
+          <div class="flex flex-col sm:flex-row justify-between items-center sm:items-start py-3 border-t border-gray-300 last:border-none">
+            <span class="w-full sm:w-1/3 font-medium text-center sm:text-left">
+              Agent
+            </span>
+            <span class="flex-1 text-center sm:text-right">
+              Abraham Lincoln
+            </span>
+          </div>
+          <div class="flex flex-col sm:flex-row justify-between items-center sm:items-start py-3 border-t border-gray-300 last:border-none">
+            <span class="w-full sm:w-1/3 font-medium text-center sm:text-left">
+              Auctioneer
+            </span>
+            <span class="flex-1 text-center sm:text-right">
+              George Washington
+            </span>
+          </div>
+          <div class="flex flex-col sm:flex-row justify-between items-center sm:items-start py-3 border-t border-gray-300 last:border-none">
+            {/**
+             * using brackets and a ternary you can add logic directly inside the dom elements
+             *
+             * [truthy variable] ? [show if true] : [show if false]
+             *
+             * null is basically show nothing
+             *
+             */}
+            {preview ? (
+              <Button colour="red">
+                <Link to="/property/testproperty"> View Listing </Link>
+              </Button>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/views/Properties/PropertyListings.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+
+import propertyData from "./PropertyData.json";
+import Property from "./Property";
+
+import Properties from "../../components/Properties/Properties.js";
+import Slideover from "../../components/Slideover/Slideover.js";
+const now = Date.now();
+
+export default function PropertyListings(props) {
+  const { setOverrideTitle, match } = props;
+
+  const [propertyPreview, setPropertyPreview] = useState(false);
+  const [currentPreviewId, setCurrentPreviewId] = useState(null);
+
+  const [currentTime, setCurrentTime] = useState(Date.now());
+  const [mode, setMode] = useState(null);
+
+  const inMins = (min) => now + 1000 * 60 * min;
+  const inHours = (hours) => now + 1000 * 60 * 60 * hours;
+
+  const auctionTimes = [
+    inMins(0),
+    inMins(1),
+    inMins(1),
+    inMins(13),
+    inMins(56),
+    inHours(3),
+  ];
+
+  useEffect(() => {
+    if (["all", "past", "upcoming", "live"].includes(match.params.mode)) {
+      const newMode = match.params.mode;
+      setOverrideTitle(
+        `${newMode[0].toUpperCase() + newMode.slice(1)} ${
+          newMode === "all" ? "properties" : "auctions"
+        }`
+      );
+      setMode(newMode);
+    }
+
+    return () => {
+      //reset override title
+      setOverrideTitle(null);
+    };
+  }, [match.params.mode]);
+
+  const handlePropertyPreview = (propertyId) => {
+    setCurrentPreviewId(propertyId);
+    setPropertyPreview(true);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => setCurrentTime((t) => t + 1000), 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const properties = Object.values(propertyData)
+    .filter((property) => {
+      if (mode === "past") {
+        return !!property.auctionCompleted;
+      } else if (mode === "upcoming") {
+        return !property.auctionLive && !property.auctionCompleted;
+      } else if (mode === "live") {
+        return property.auctionLive;
+      }
+
+      return true;
+    })
+    .map((property, index) => {
+      return {
+        ...property,
+        auctionTime: auctionTimes[index],
+      };
+    });
+
+  return (
+    <>
+      <Properties
+        onPropertyPreview={handlePropertyPreview}
+        properties={properties}
+        currentTime={currentTime}
+      />
+      <Slideover
+        open={propertyPreview}
+        setOpen={setPropertyPreview}
+        title="Property Preview"
+      >
+        <Property {...props} preview={true} propertyId={currentPreviewId} />
+      </Slideover>
+    </>
+  );
+}
