72cd514f51c6cc440c30c891debaa952f628fd1f
s-li1 <stevenli8892@hotmail.com.au>
2021-09-23 15:09:46 +1000

    should configure auction endpoint

--- backend/routes.js
+++ backend/routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
 const properties = require("./services/Properties.js");
+const AuctionController = require("./controllers/AuctionsController");
 
 router.use("/properties", properties);
+
+router.use("/auction", AuctionController.show);
 
 module.exports = router;
