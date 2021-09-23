bbffcd1c130299f720d84c01aa47d3bd889a2441
s-li1 <stevenli8892@hotmail.com.au>
2021-09-23 15:08:20 +1000

    should remove commented out code

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const routes = require("./routes.js");
-
-//const db=('./db');
-
-app.use(express.static("../frontend/build"));
 const mysql = require("mysql");
 const dbconfig = require("./db.config");
+
+app.use(express.static("../frontend/build"));
+
+app.use('/api', routes);
 
 const connection = mysql.createConnection({
   host: dbconfig.host,
@@ -16,20 +16,6 @@ const connection = mysql.createConnection({
   password: dbconfig.password,
   database: dbconfig.database,
 });
-
-//open the MySQL connection
-/*connection.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
-
-
-  const set = "CREATE TABLE users (user_id INT, First_Name VARCHAR(255), Last_Name VARCHAR(255),User_Role VARCHAR(255),Password VARCHAR(255),Email VARCHAR(255))";
-  connection.query(set, function (err, result) {
-    if (err) throw err;
-    console.log("Ticket Table created");
-  });
-
- });*/
 
 app.listen(port, () => {
   console.log(`ASD app listening on ${port}`);
