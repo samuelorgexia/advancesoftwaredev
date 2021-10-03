8f1070493ab581d7f3ade43c9f7032430b7cc04d
s-li1 <stevenli8892@hotmail.com.au>
2021-10-03 21:00:42 +1100

    basic socket server initialization

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 const routes = require("./routes.js");
 const mysql = require("mysql");
@@ -17,6 +21,10 @@ const connection = mysql.createConnection({
   database: dbconfig.database,
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(port, () => {
   console.log(`ASD app listening on ${port}`);
 });
