7773be019d7ba418fde4efb727e6cb23d4350149
s-li1 <stevenli8892@hotmail.com.au>
2021-10-05 21:51:03 +1100

    added auction component

---
+++ frontend/src/views/Auction/Auction.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import Button from "../../components/Buttons/Button";
+import { io } from "socket.io-client";
+const socket = io("localhost:5000");
+
+export default function Auction(props) {
+  const [userBid, setUserBid] = useState(0);
+  const [highestBid, setHighestBid] = useState(0);
+
+  useEffect(()=> {
+    socket.on('bid:read', (currentBid) => {
+      setHighestBid(currentBid);
+    });
+  },[]);
+
+  const handleBidSubmit = ()=> {
+      socket.emit("bid:create", userBid);
+  }
+
+  return (
+    <div>
+      <h1>Current Bid: <span>${highestBid.toLocaleString()}</span></h1>
+      $ <input type="number" min="1" placeholder="Input your bid" onChange={e => setUserBid(e.target.valueAsNumber)}/>
+      <Button onClick={handleBidSubmit}>Place Bid</Button>
+    </div>
+  );
+}
