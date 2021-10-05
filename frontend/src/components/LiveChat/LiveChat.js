3bb78b742e54715e75e4b7688cb276b03ed31176
crispylotus <sonnyly1@outlook.com>
2021-10-05 22:39:30 +1100

    updated chat ui

--- frontend/src/components/LiveChat/LiveChat.js
+++ frontend/src/components/LiveChat/LiveChat.js
@@ -45,13 +45,6 @@ function LiveChat( {socket, username, room}) {
                             <span className="text-lg text-gray-600"> </span>
                         </div>
                     </div>
-                    <div className="flex items-center space-x-2">
-                        <button type="button" className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                            </svg>
-                        </button>
-                    </div>
                 </div>
                 <div className="chat-body">
                     <ScrollToBottom className="message-container">
@@ -62,9 +55,9 @@ function LiveChat( {socket, username, room}) {
                                         <div className="message-content">
                                                 <p>{messageContent.message}</p>
                                         </div>
-                                        <div className="message-data">
-                                            <p className="">{messageContent.time} {messageContent.author}</p> 
-                                            
+                                        <div className="message-meta">
+                                            <p id="time">{messageContent.time}</p>
+                                            <p id="author">{messageContent.author}</p>
                                         </div>
                                     </div>
                                 </div>
@@ -73,8 +66,35 @@ function LiveChat( {socket, username, room}) {
                     </ScrollToBottom>
                 </div>
             </div>
+            <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+                    <div className="relative flex">
 
-            <div>
+                            <input 
+                                className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-full py-3"
+                                placeholder="Write Something" 
+                                type="text" 
+                                value={currentMessage}
+                                placeholder="Write Something"
+                                onChange={(event) => {
+                                    setCurrentMessage(event.target.value);
+                                }}
+                                onKeyPress={(event) => {
+                                    event.key === "Enter" && sendMessage();
+                                }}
+                            />
+                        <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
+                            <button 
+                                onClick={sendMessage}
+                                type="button" 
+                                className="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 transform rotate-90">
+                                <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                            </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            {/* <div>
                 <input 
                     type="text" 
                     value={currentMessage}
@@ -87,7 +107,7 @@ function LiveChat( {socket, username, room}) {
                     }}
                 />
                 <button onClick={sendMessage}>&#9658;</button>
-            </div>
+            </div> */}
 
             {/* <div className="flex-1 justify-between flex flex-col h-screen">
                 <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
@@ -141,8 +161,13 @@ function LiveChat( {socket, username, room}) {
                                 className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-full py-3"
                                 placeholder="Write Something" 
                                 type="text" 
+                                value={currentMessage}
+                                placeholder="test.."
                                 onChange={(event) => {
                                     setCurrentMessage(event.target.value);
+                                }}
+                                onKeyPress={(event) => {
+                                    event.key === "Enter" && sendMessage();
                                 }}
                             />
                         <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
