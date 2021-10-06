15a900932fc8f421674e728a8afc7d980cb3d4c1
s-li1 <stevenli8892@hotmail.com.au>
2021-10-07 10:17:28 +1100

    create bid handler

---
+++ backend/handlers/bidHandler.js
@@ -0,0 +1,14 @@
+module.exports = (io, socket) => {
+    let currentBid = 1000000;
+
+    const createBid = (bid) => {
+        if (bid > currentBid) {
+            currentBid = bid;
+        }
+
+        io.emit("bid:read", currentBid);
+    }
+
+    socket.on("bid:create", createBid);
+    io.emit("bid:read", currentBid);
+}
