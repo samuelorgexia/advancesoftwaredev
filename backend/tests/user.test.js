285c65f18463e8e3e11b2a5d2c6a54880718f68e
Samuel Li <samuelgh.li@gmail.com>
2021-10-07 18:30:41 +1100

    env added

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -1,4 +1,14 @@
+// signup 
+const testData=[
+    {firstName:"",lastName:"",email:"",password:""},
+    {firstName:"Samuel",lastName:"Li",email:"",password:""},
+    {firstName:"Samuel",lastName:"Li",email:"@gmail.com",password:"123"},
+    {firstName:"",lastName:"",email:"",password:""},
+];
 
+for(var i in testData){
+    console.log(testData[i]);
+}
 
 
 describe('Sample Test', () => {
